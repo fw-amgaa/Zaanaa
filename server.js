579ac9fc621fd89c5ae8255ab274e4954bcbcecb
mnkhod <eikyuu.hoshi@gmail.com>
2020-04-26 16:18:45 +0800

    Adding All Essential Initial Files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require("path")
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static('prod'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'prod','home.html')));
+app.get('/products', (req, res) => res.sendFile(path.join(__dirname,'prod','products.html')));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
