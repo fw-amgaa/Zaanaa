99b4397597ba1506b0cf8d35098e19de2797691d
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-02 02:38:53 +0800

    HARD RESET : Using Express Application Generator

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('prod'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'prod','home.html')));
+app.get('/', (req, res) => {
+
+  res.sendFile(path.join(__dirname,'prod','home.html'))
+
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
