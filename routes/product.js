513fde00951d439faf7b7c35a57ac9669e31c2dc
fw-amgaa <jasqwe6008@gmail.com>
2020-05-02 22:55:35 +0800

    Modified Route , Category and Single Products

--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,18 @@ var router = express.Router();
 
 // Archive
 router.get('/', function(req, res, next) {
-  res.render('archive-product');
+  res.render('archive-product', { title : 'Shop' });
 });
 
 // Single
-router.get('/:name', function(req, res, next) {
+router.get('/:categoryName/:name', function(req, res, next) {
   res.render('single-product');
 });
+
+// Category Archive
+router.get('/:categoryName', function(req, res, next) {
+  res.render('archive-product' , { title : req.params.categoryName });
+});
+
 
 module.exports = router;
